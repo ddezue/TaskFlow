dbe5cb82371f726245b69561c3cc6a7feca746d9
dezue <mistfoll@mail.ru>
2026-06-03 18:24:30 +0700

    task for test

--- TaskFlow/Task.cs
+++ TaskFlow/Task.cs
@@ -1,10 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TaskFlow {
-  internal class Task {
+  public class Task {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string DueDate { get; set; }
+    public string Priority { get; set; }
+    public TaskState State { get; set; }
+    public string AssignedTo { get; set; }
+
+    public Task(string title, string description, string dueDate, string priority) {
+      Id = Guid.NewGuid().ToString();
+      Title = title;
+      Description = description;
+      DueDate = dueDate;
+      Priority = priority;
+      State = new NewState();
+      AssignedTo = string.Empty;
+    }
+
+    public Task Clone() {
+      return new Task(Title, Description, DueDate, Priority) {
+        AssignedTo = AssignedTo,
+        State = State.Clone()
+      };
+    }
   }
 }
