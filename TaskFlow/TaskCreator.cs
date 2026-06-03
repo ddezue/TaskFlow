d948f21fd651a1693fe19bd548b79eb74c92b7ba
Egor12312 <uegor6705@gmail.com>
2026-06-03 19:27:01 +0700

    added another class

---
+++ TaskFlow/TaskCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TaskFlow {
+  public class TaskCreator {
+    private readonly List<TaskTemplate> _templates;
+
+    public TaskCreator() {
+      _templates = new List<TaskTemplate>();
+    }
+
+    public void AddTemplate(TaskTemplate template) {
+      _templates.Add(template);
+    }
+
+    public Task CreateFromTemplate(int templateIndex) {
+      if (templateIndex < 0 || templateIndex >= _templates.Count) {
+        return null;
+      }
+      return _templates[templateIndex].CreateTask();
+    }
+
+    public Task CloneTask(Task original) {
+      return original.Clone();
+    }
+  }
+}
