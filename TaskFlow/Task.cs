3f8a59add6317093fa38c9d11a50350b8b0863b9
Egor12312 <uegor6705@gmail.com>
2026-06-03 17:57:50 +0700

    added the class framework and properties

---
+++ TaskFlow/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskFlow {
+  public class Task {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string DueDate { get; set; }
+    public string Priority { get; set; }
+    public string Status { get; set; }
+    public string AssignedTo { get; set; }
+
+  }
+}
