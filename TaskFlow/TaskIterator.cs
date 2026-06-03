69fbddb8671be267090c26fbca05a022ff018b83
Kristina Shcherbakova <kristinagrom876@gmail.com>
2026-06-03 18:52:51 +0700

    add AssignCurrentTo method for task assignment

--- TaskFlow/TaskIterator.cs
+++ TaskFlow/TaskIterator.cs
@@ -1,40 +1,52 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-public class TaskIterator {
-  private List<Task> _tasks;
-  private int _currentIndex;
+namespace TaskFlow {
+  public class TaskIterator {
+    private readonly List<Task> _tasks;
+    private int _currentIndex;
 
-  public TaskIterator(List<Task> tasks) {
-    _tasks = tasks;
-    _currentIndex = 0;
-  }
-}
-public bool HasNext() {
-  return _currentIndex < _tasks.Count - 1;
-}
+    public TaskIterator(List<Task> tasks) {
+      _tasks = tasks;
+      _currentIndex = 0;
+    }
 
-public bool HasPrevious() {
-  return _currentIndex > 0;
-}
-public Task Next() {
-  if (HasNext()) {
-    _currentIndex++;
-    return _tasks[_currentIndex];
-  }
-  return null;
-}
+    public bool HasNext() {
+      return _currentIndex < _tasks.Count - 1;
+    }
 
-public Task Previous() {
-  if (HasPrevious()) {
-    _currentIndex--;
-    return _tasks[_currentIndex];
+    public bool HasPrevious() {
+      return _currentIndex > 0;
+    }
+
+    public Task Next() {
+      if (HasNext()) {
+        ++_currentIndex;
+        return _tasks[_currentIndex];
+      }
+      return null;
+    }
+
+    public Task Previous() {
+      if (HasPrevious()) {
+        --_currentIndex;
+        return _tasks[_currentIndex];
+      }
+      return null;
+    }
+
+    public Task GetCurrent() {
+      if (_tasks == null || _tasks.Count == 0) {
+        return null;
+      }
+      return _tasks[_currentIndex];
+    }
+
+    public void AssignCurrentTo(string userName) {
+      Task current = GetCurrent();
+      if (current != null) {
+        current.AssignedTo = userName;
+      }
+    }
   }
-  return null;
-}
-public Task GetCurrent() {
-  if (_tasks == null || _tasks.Count == 0) {
-    return null;
-  }
-  return _tasks[_currentIndex];
 }
