42b1a46ff4914922de3d5c94731e046cbdbeecdd
Kristina Shcherbakova <kristinagrom876@gmail.com>
2026-06-03 17:48:22 +0700

    add TaskIterator class skeleton and navigation

---
+++ TaskFlow/TaskIterator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class TaskIterator {
+  private List<Task> _tasks;
+  private int _currentIndex;
+
+  public TaskIterator(List<Task> tasks) {
+    _tasks = tasks;
+    _currentIndex = 0;
+  }
+}
+public bool HasNext() {
+  return _currentIndex < _tasks.Count - 1;
+}
+
+public bool HasPrevious() {
+  return _currentIndex > 0;
+}
